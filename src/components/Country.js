516ed3dee66c2af4d02ced0b05210e3526205002
rodrigo <rodrigo_coelho@orange.fr>
2022-04-30 12:47:54 +0200

    add back button in Country component

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Country = (props) => {
     const { country } = props;
 
     return (
-        <div>{country.name}</div>
+        <div>
+            <Link to="/">back</Link>
+            <h1>{country.name}</h1>
+        </div>
     )
 }
 
